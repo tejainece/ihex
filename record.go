709da6e4170c09a3ce82747b35d7905c39f1ee33
Ravi Teja Gudapati <tejainece@gmail.com>
2016-07-09 16:11:26 +0200

    Record file consolidation

--- record.go
+++ record.go
@@ -1,6 +1,7 @@
 package ihex
 
 import (
+	"encoding/hex"
 	"github.com/tejainece/hexutils"
 )
 
@@ -108,4 +109,21 @@ func (meRec *Record) MakeRecord(aIn []byte) (*Record, error) {
 		addr:    lAddr,
 		recType: lType,
 	}, nil
+}
+
+func (meRec Record) GetData() (uint16, []byte, error) {
+	//Check type
+	if meRec.recType != RecTypeData {
+		return 0, nil, ErrInvalidRecType
+	}
+
+	lRet := make([]byte, meRec.length)
+
+	lConved, lErr := hex.Decode(lRet, meRec.bytes[9:len(meRec.bytes)-2])
+
+	if lConved != len(lRet) || lErr != nil {
+		return 0, nil, ErrNonHexContent
+	}
+
+	return meRec.addr, lRet, nil
 }
